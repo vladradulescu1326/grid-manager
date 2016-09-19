90e15e4d6f2999b804e19b0b1ce04556773a25a5
Catalin Mustata <catalin.mustata@mindit.ro>
2016-09-19 17:22:22 +0300

    Configured everything through a config object

--- gridManager.js
+++ gridManager.js
@@ -9,23 +9,21 @@ angular.module('gridManager',[]).service('gridManager', function($compile) {
     //Filter Template
     var _filterTemplate = "<input type='text' class='table-filter'></input>";
 
-    function GridManager(scope, gridOptions, tableId, doesNeedSelectAll, doesNeedQuickFilter, quickFilterPlaceHolder, customOptions) {
+    function GridManager(gridOptions, config, customOptions) {
         var self = this;
 
-        var _gridOptions = {};
-        var _doesNeedSelectAll = doesNeedSelectAll;
-        var _doesNeedQuickFilter = doesNeedQuickFilter;
+        var _gridOptions;
+        var _doesNeedSelectAll;
+        var _doesNeedQuickFilter;
         var _ignoreSelectEventFlag = false;
-        var _scope = scope;
-        var _tableID = tableId;
+        var _scope;
+        var _tableID;
         var _selectAllModel = "";
-        var _quickFilterPlaceHolder = quickFilterPlaceHolder;
+        var _quickFilterPlaceHolder
 
         var _clickRowCallback;
         var _selectRowCallback;
         var _changedSelectionCallback;
-
-        self.selectAll = false;
 
         if (angular.isUndefined(gridOptions)) {
             console.log("Did not receive a valid gridOptions parameter. Aborting.");
@@ -34,14 +32,32 @@ angular.module('gridManager',[]).service('gridManager', function($compile) {
 
         _gridOptions = gridOptions;
 
+        if (angular.isDefined(config.scope)) {
+            _scope = config.scope;
+        }
+
+        if (angular.isDefined(config.tableId)) {
+            _tableID = config.tableId;
+        }
+
+        if (angular.isDefined(config.doesNeedQuickFilter)) {
+            _doesNeedQuickFilter = config.doesNeedQuickFilter;
+
+            if (angular.isDefined(config.quickFilterPlaceHolder)) {
+                _quickFilterPlaceHolder = config.quickFilterPlaceHolder;
+            }
+        }
+
         _gridOptions.onGridReady = _gridReady;
         _gridOptions.onRowSelected = _didSelectRow;
         _gridOptions.onSelectionChanged = _didChangeSelection;
         _gridOptions.onRowClicked = _didClickRow;
 
-        _doesNeedSelectAll = doesNeedSelectAll;
+        if (angular.isDefined(config.doesNeedSelectAll)) {
+            _doesNeedSelectAll = config.doesNeedSelectAll;
+        }
 
-        if (doesNeedSelectAll) {
+        if (_doesNeedSelectAll) {
             _gridOptions.columnDefs.unshift({headerName: "", checkboxSelection: true, width: 20, suppressMenu: true, headerCellTemplate: _selectAllContainer});
         }
 
